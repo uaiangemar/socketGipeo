9a22633e7622a09aa45eb0723f74d9e8317f1ac5
Angemar <angemar@autonomadeica.edu.pe>
2024-05-05 21:11:24 -0500

    cors v4

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,8 @@ router.post('/subscription', async ( req, res ) => {
         "OPTIONS, GET, POST, PUT, PATCH, DELETE"
       );
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    console.log(req.body);
-    pushSubscriptionClient = req.body;
+    await console.log('body: ', req.body);
+    pushSubscriptionClient = await req.body;
     // res.status(200).json();
 
     const payload = 
