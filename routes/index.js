49017e8b8f2b089d2005e5bf1521410bb9fb9be4
Angemar <angemar@autonomadeica.edu.pe>
2024-05-07 22:36:25 -0500

    cord 8

--- routes/index.js
+++ routes/index.js
@@ -14,7 +14,7 @@ router.post('/subscription', ( req, res, next ) => {
         "OPTIONS, GET, POST, PUT, PATCH, DELETE"
       );
 
-    console.log('body: ', req);
+    console.log('body: ', req.body);
     pushSubscriptionClient = req.body;
     // res.status(200).json();
     res.status(200).json({message: 'Newsletter sent successfully.'})
